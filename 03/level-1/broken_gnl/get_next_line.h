548f88650cfc0eddc05889a2b651352ca3b69b7a
Tristan Ortiz Roset <tortiz-r@c1r15s2.42madrid.com>
2025-10-16 22:12:35 +0200

    added given .h file to 03/broken_GNL

---
+++ 03/level-1/broken_gnl/get_next_line.h
@@ -0,0 +1,12 @@
+//This file is given at the exam as it is (incomplete and/or incorrect)
+
+#ifndef GNL
+#define GNL
+
+#ifndef BUFFER_SIZE
+#define BUFFER_SIZE 10
+#endif
+
+char	*get_next_line(int fd);
+
+#endif
