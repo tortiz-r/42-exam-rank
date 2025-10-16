a0b5138db9a81fc026b16ac8711dfcbb45754d31
Tristan Ortiz Roset <tortiz-r@c1r15s2.42madrid.com>
2025-10-16 22:14:09 +0200

    added given get_next_line.c file (nearly empty yet)

---
+++ 03/level-1/broken_gnl/get_next_line.c
@@ -0,0 +1,10 @@
+//This file is given at the exam as it is (incomplete and/or incorrect)
+
+#include "get_next_line.h"
+
+// pdte-> transcribir esta función del subject del examen
+char	*get_next_line(int fd);
+
+// resto de funciones
+	// (con código, pdte transcribirlo del subject del examen)
+
